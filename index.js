31ab9b35270fef470273922c2fa03e18b8a889bc
JoaoOliveiraZz <linajoaovictor@gmail.com>
2022-04-11 20:15:17 -0300

    alterações no css e adicionada a função de trocar a cor da borda

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const url = 'https://api.github.com/users/birobirobiro'
-const button = document.querySelector('.btn')
+const url = 'https://api.github.com/users/birobirobiro';
+const button = document.querySelector('.btn');
+const colors = ['red', 'green', 'blue', 'purple', 'aquamarine', 'pink', 'gold'];
+
+button.addEventListener('click', () =>{
+
+    var random = Math.floor(Math.random() * 7);
+    parseInt(random);
+
+    document.getElementById('card').style.border = `10px solid ${colors[random]}`;
+
+})
 
 fetch(url).then(
     response => response.json()
 ).then(
     data =>{
-        writeData(data)
+        writeData(data);
     }
 ).catch(
     error => console.error(error)
@@ -13,11 +23,11 @@ fetch(url).then(
 
 function writeData (dados){
     document.querySelector('.user').textContent = dados.login;
-    document.querySelector('.seguidores').textContent = dados.followers;
-    document.querySelector('.seguindo').textContent = dados.following
-    document.querySelector('.repositorios').textContent = dados.public_repos
-    document.querySelector('.company').textContent = dados.company
-    document.querySelector('.localizacao').textContent = dados.location
+    document.querySelector('.seguidores').textContent = ` ${dados.followers} Seguidores`;
+    document.querySelector('.seguindo').textContent = `  ${dados.following} Seguindo`;
+    document.querySelector('.repositorios').textContent = `  ${dados.public_repos} repositórios`;
+    document.querySelector('.company').textContent = dados.company;
+    document.querySelector('.localizacao').textContent = dados.location;
 }
 
 
